31ac0775e0b8ef3d0bb55cd77c3da865243e7c59
Elad Ben-Israel <elad.benisrael@gmail.com>
2012-02-10 01:01:09 +0200

    update build

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #/bin/bash
-git remote add out git@github.com:eladb/test-eladb.github.com.git || exit 1
+
+# add the output repository and rebase to it
+USERNAME=eladb
+PASSWORD=XXX
+REPO=https://$USERNAME:$PASSWORD@github.com/eladb/test-eladb.github.com.git
+git remote add out $REPO || exit 1
 git fetch out || exit 1
 git rebase out/master || exit 1
+
+# now build the website
 node_modules/.bin/jade *.jade || exit 1
+
+# push to the output repo
+git add . || exit 1
+git commit -am 'build results' || exit 1
+git push out master || exit 1
 
